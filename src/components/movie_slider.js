5de953129ac168b99336dd599cab58cdc5943952
navib <ivan.baena.dev@gmail.com>
2018-02-12 01:21:51 -0500

    slider added to home

--- src/components/movie_slider.js
+++ src/components/movie_slider.js
@@ -5,26 +5,33 @@ class MovieSlider extends Component {
   componentDidMount(){
     var mySwiper = new Swiper('.swiper-container', {
       direction: 'vertical',
-      loop: true,
-      navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
+      mousewheel: {
+        invert: false,
+        releaseOnEdges: true,
+        sensitivity: 1
       },
+      pagination: {
+        el: '.swiper-pagination',
+      },
+      on: {
+        reachEnd: function() {
+          console.log("reached end! : ", window.pageYOffset);
+          //document.querySelector('.scroll-content').style.transform = 'translate3d(0px, -682px, 0px)';
+
+        }
+      }
      });
   }
   render() {
     return (
       <div className="movie-slider-wrapper">
-        <div class="swiper-container">
-          <div class="swiper-wrapper">
-            <div class="swiper-slide">Slide 1</div>
-            <div class="swiper-slide">Slide 2</div>
-            <div class="swiper-slide">Slide 3</div>
+        <div className="swiper-container">
+          <div className="swiper-wrapper">
+            <div className="swiper-slide">Slide 1</div>
+            <div className="swiper-slide">Slide 2</div>
+            <div className="swiper-slide">Slide 3</div>
           </div>
-          <div class="swiper-pagination"></div>
-
-          <div class="swiper-button-prev"></div>
-          <div class="swiper-button-next"></div>
+          <div className="swiper-pagination"></div>
         </div>
       </div>
     )
